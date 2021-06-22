69018c70ad87cb6c08edbad8c00f6fde5c5029c8
Chafik LAGHRISSI <laghrissichafik@gmail.com>
2021-06-22 20:43:00 +0100

    modify image downloader service

--- services/ImageDownloader.py
+++ services/ImageDownloader.py
@@ -5,9 +5,11 @@ import os
 from pathlib import Path
 from fastapi import Response, status
 from starlette import responses
+PASSWORD = "9Auy@qznE!LbxQL"
+NAME = "creastation"
 
 
-def handleImage(date: str, location: dict, cloudCoverage: float, userName: str, response: Response):
+def handleImage(date: str, location: dict, cloudCoverage: float, userId: int):
     """
     This function take six arguments:
     date: (type: string) min date intervale
@@ -16,7 +18,7 @@ def handleImage(date: str, location: dict, cloudCoverage: float, userName: str, 
     userName:(type: string) current user name
 
     """
-    api = SentinelAPI(os.getenv('NAME'), os.getenv("PASSWORD"))
+    api = SentinelAPI(NAME, PASSWORD)
     area = {
 
         "type": "Point",
@@ -27,14 +29,17 @@ def handleImage(date: str, location: dict, cloudCoverage: float, userName: str, 
 
     area = geojson_to_wkt(area)
     result = api.query(area=area, date=(date, "NOW"), order_by="cloudcoverpercentage", cloudcoverpercentage=(
-        0, cloudCoverage), orbitdirection='DESCENDING', limit=1, platformname='Sentinel-1')
+        0, cloudCoverage), orbitdirection='DESCENDING', limit=1, platformname='Sentinel-3')
     if result:
         id = list(result.keys())[0]
-        print(id)
-        path = f"./DB/images/{userName}"
-        return {**Path(path).mkdir(parents=True, exist_ok=True), "status": "success"}
+        path = f"./DB/images/{userId}"
+        imageName = result[id]["title"]+".jpeg"
+        Path(path).mkdir(parents=True, exist_ok=True)
+        api.download_quicklook(id, path)
+        return {"path": path.replace("./",""), "imageName": imageName}
 
-        return
     else:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return{"error": "we couldn't found image for given criteria"}
+        raise Exception("we couldn't find image for the given criteria")
+
+
+print(handleImage("20151202", {"lon": -5, "lat": 39}, 15, 20))
