f5b98baed789d4c031bce265cf0cc0d6bfa7d9bb
Hamza Mogni <hamzamogni5@gmail.com>
2021-06-25 13:17:40 +0100

    clean up users routes file

--- routes/user.py
+++ routes/user.py
@@ -1,14 +1,15 @@
 from fastapi import HTTPException
 from DB.database import get_db
 from sqlalchemy.orm.session import Session
-from schemas.User import UserCreate
 from starlette import status
 from schemas.Image import UserViewer
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from models.User import User
 from passlib.context import CryptContext
 from services.auth_services import get_current_user
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 router = APIRouter(tags=["users"],
                    prefix="/user")
 
@@ -20,36 +21,58 @@ def get_current_user(current_user: User = Depends(get_current_user)):
         return current_user
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Oops somthing went wrong, try to loggin again! ")
+                            detail="Couldn't login")
 
 
 @router.put("/", response_model=UserViewer)
-def update_email_and_password(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+def update_email_and_password(
+                        id: int,
+                        db: Session = Depends(get_db),
+                        current_user: User = Depends(get_current_user)
+                    ):
     user = db.query(User).filter(User.id == id).first()
+
     if user:
         return user
+
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"There's no user with the given {id}")
 
 
 @router.patch("/", response_model=UserViewer)
-def update_email_or_password(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+def update_email_or_password(
+                        id: int,
+                        db: Session = Depends(get_db),
+                        current_user: User = Depends(get_current_user)
+                    ):
     user = db.query(User).filter(User.id == id).first()
+
     if user:
         return user
+
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"There's no user with the given {id}")
 
 
 @router.delete("/", response_model=UserViewer)
-def delete_account(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+def delete_account(
+                id: int,
+                db: Session = Depends(get_db),
+                current_user: User = Depends(get_current_user)
+            ):
     pass
 
 
 @router.get("/user/{id}", response_model=UserViewer)
-def get_current_user(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+def get_current_user(
+                id: int,
+                db: Session = Depends(get_db),
+                current_user: User = Depends(get_current_user)
+            ):
     user = db.query(User).filter(User.id == id).first()
+
     if user:
         return user
+
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail=f"R=There's no user with the given {id}")
+                        detail=f"There's no user with the given {id}")
