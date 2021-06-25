f3d3fe400fa961a262c429e023744db48c921b32
Hamza Mogni <hamzamogni5@gmail.com>
2021-06-25 13:12:03 +0100

    clean up ImageDownlaoder Service

--- services/ImageDownloader.py
+++ services/ImageDownloader.py
@@ -24,19 +24,31 @@ def download_image(userId: int, imageId: int, imageTitle: str):
     return {"path": path.replace("./", ""), "imageName": imageName}
 
 
-def handle_image_information(date: str, location: dict, cloudCoverage: float, platformname: str = "Sentinel-1", ):
+def handle_image_information(
+            date: str,
+            location: dict,
+            cloudCoverage: float,
+            platformname: str = "Sentinel-1"
+        ):
     """
     This function take 4 arguments:
+
     date: (type: string) min date intervale
-    location: (tye: dictionary) containing two keys longitude and latitude (type: float).
-    cloudCoverage:(type: float) pourcentage of cloud coverage of the image looked for.
-    platformname:(type: string) choose the platform which you want to downlow sat images from.
-    for example:
-         Sentinel-1
-         Sentinel-2
-         Sentinel-3
-         Sentinel-4
-    default Sentinel-1
+
+    location: (tye: dictionary) containing two keys
+                longitude and latitude (type: float).
+
+    cloudCoverage: (type: float) pourcentage of cloud
+                      coverage of the image looked for.
+
+    platformname: (type: string) choose the platform which
+                    you want to downlow sat images from.
+                    for example:
+                         Sentinel-1
+                         Sentinel-2
+                         Sentinel-3
+                         Sentinel-4
+                    default Sentinel-1
     """
     area = {
         "type": "Point",
@@ -46,16 +58,30 @@ def handle_image_information(date: str, location: dict, cloudCoverage: float, pl
     }
 
     area = geojson_to_wkt(area)
-    result = api.query(area=area, date=(date, "NOW"), order_by="cloudcoverpercentage", cloudcoverpercentage=(
-        0, cloudCoverage), orbitdirection='DESCENDING', limit=1, platformname=platformname)
+
+    result = api.query(
+                    area=area,
+                    date=(date, "NOW"),
+                    order_by="cloudcoverpercentage",
+                    cloudcoverpercentage=(0, cloudCoverage),
+                    orbitdirection='DESCENDING',
+                    limit=1,
+                    platformname=platformname)
+
     if result:
         id = list(result.keys())[0]
         iterator = dict(result[id])
-        res = {key: iterator[key] for key in iterator.keys() and {
-            "summary", "title", "platformname", "size", "cloudcoverpercentage"}}
+        res = {
+                key: iterator[key] for key in iterator.keys()
+                and {
+                        "summary",
+                        "title",
+                        "platformname",
+                        "size",
+                        "cloudcoverpercentage"
+                    }
+                }
+
         return {**res, "id": id}
     else:
         raise Exception("we couldn't find image for the given criteria")
-result = handle_image_information("20001202", {"lon": -5, "lat": 36.44}, 100,platformname="Sentinel-3")
-print(result)
-print(download_image(1, result["id"], result["title"]))
