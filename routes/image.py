a77b3ca0b1d174b5a40cfac8741eb2dbfba92679
Hamza Mogni <hamzamogni5@gmail.com>
2021-06-27 13:16:29 +0100

    make it so that we don't download the image from sentinel if it already exists in our filesystem

--- routes/image.py
+++ routes/image.py
@@ -1,19 +1,16 @@
 import os
-from pathlib import Path
 
 from fastapi.responses import FileResponse
 from services.UserService import update_image_status
 from services.auth_services import get_current_user
 from services.ImageDownloader import download_image as download
-from fastapi import HTTPException, Depends, APIRouter
+from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from DB.database import get_db
 from models.Image import Image
-from models.User import User
-from schemas.Image import Image as Im
 
 router = APIRouter(
-    tags=["images"],
+    tags=["Images"],
     prefix="/images",
     # dependencies=[Depends(get_current_user)]
 )
@@ -31,7 +28,13 @@ def dowload_image(name: str, db: Session = Depends(get_db)):
         image = db.query(Image).filter(Image.name == name).first()
         path = os.getcwd() + image.path.replace("./", "/")
 
+        if not os.path.exists(path):
+            download(image.user_id, image.product_id, image.name)
+
+        update_image_status(db, image.id, True)
+
         return FileResponse(path, filename=f"{image.name}.zip")
 
     except Exception as error:
+        update_image_status(db, image.id, False)
         return error
